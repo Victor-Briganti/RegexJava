328c5b3b580ecfcd47bdb0eef8766703aaa0a0e6
Victor Briganti <jvbriganti@gmail.com>
2024-08-20 20:24:21 -0300

    State class implemented

---
+++ src/main/java/org/state/State.java
@@ -0,0 +1,112 @@
+package org.state;
+
+import java.util.ArrayList;
+
+import org.matcher.PatternMatcher;
+
+/**
+ * Represents a state that in the automaton. The state is used to match the
+ * current symbol and transition to the next valid state.
+ */
+public class State {
+
+    /**
+     * Represents a transition in the automaton. A transition defines how the
+     * automaton moves from one state to the other based on the specific pattern
+     * matching.
+     */
+    public static class Transition {
+        private State state;
+        private PatternMatcher patternMatcher;
+
+        /**
+         * Constructs a Transition with the specified target state and pattern matcher.
+         * 
+         * @param state          the state to transition to. Should not be null
+         * @param patternMatcher the pattern matcher that defines the transition
+         *                       condition. Should not be null
+         */
+        public Transition(State state, PatternMatcher patternMatcher) {
+            assert state != null : "The next state cannot be null";
+            assert patternMatcher != null : "The pattern to be matched cannot be null";
+            this.state = state;
+            this.patternMatcher = patternMatcher;
+        }
+
+        /**
+         * Getter for the next state of the transtion
+         * 
+         * @return the state to wich this transition lead.
+         */
+        public State getState() {
+            return state;
+        }
+
+        /**
+         * Getter for the pattern matcher that specifies the condition for the
+         * transition.
+         * 
+         * @return the pattern matcher associated with this transition.
+         */
+        public PatternMatcher getPatternMatcher() {
+            return patternMatcher;
+        }
+    }
+
+    // Represents the name of the states 'q0', 'q1' ... 'qn'
+    // Used only for debugging purposes.
+    private String name;
+
+    // List with all the accepted transitions that this state have.
+    private ArrayList<Transition> transitions;
+
+    /**
+     * Constructor of the state
+     * 
+     * @param name the name of the state
+     */
+    public State(String name) {
+        this.name = name;
+        transitions = new ArrayList<Transition>();
+    }
+
+    /**
+     * Adds a transition from this state to a specified target state.
+     * The new transition is appended to the end of the transition list.
+     * 
+     * @param state          the target state for the transition
+     * @param patternMatcher the pattern matcher defining the transition condition.
+     */
+    public void addTransition(State state, PatternMatcher patternMatcher) {
+        transitions.add(new Transition(state, patternMatcher));
+    }
+
+    /**
+     * Adds a transition from this state to a specified target state.
+     * The new transition is prepend to the end of the transition list.
+     * 
+     * @param state          the target state for the transition
+     * @param patternMatcher the pattern matcher defining the transition condition.
+     */
+    public void addHighestTransition(State state, PatternMatcher patternMatcher) {
+        transitions.add(0, new Transition(state, patternMatcher));
+    }
+
+    /**
+     * Getter for the list of transitions of this state
+     * 
+     * @return a list of transitions associated with this state
+     */
+    public ArrayList<Transition> getTransitions() {
+        return transitions;
+    }
+
+    /**
+     * Getter for the name of the state
+     * 
+     * @return the name of the state
+     */
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/test/java/org/state/StateTest.java
@@ -0,0 +1,81 @@
+package org.state;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.matcher.CharacterMatcher;
+import org.matcher.EpsilonMatcher;
+import org.matcher.PatternMatcher;
+import org.state.State.Transition;
+
+public class StateTest {
+    State state;
+
+    @Test
+    public void stateConstructor() {
+        state = new State("q0");
+        int result = state.getName().compareTo("q0");
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void stateAddTransition() {
+        state = new State("q0");
+
+        ArrayList<State> states = new ArrayList<State>();
+        ArrayList<PatternMatcher> matchers = new ArrayList<PatternMatcher>();
+
+        for (int i = 0; i < 30; i++) {
+            String identifier = "q" + Integer.toString(i);
+
+            states.add(new State(identifier));
+            if (i == 29) {
+                matchers.add(new EpsilonMatcher());
+            } else {
+                matchers.add(new CharacterMatcher(identifier.charAt(1)));
+            }
+
+            state.addTransition(states.get(i), matchers.get(i));
+        }
+
+        ArrayList<Transition> transitions = state.getTransitions();
+        for (int i = 0; i < 30; i++) {
+            assertEquals(states.get(i), transitions.get(i).getState());
+            assertEquals(matchers.get(i), transitions.get(i).getPatternMatcher());
+        }
+
+    }
+
+    @Test
+    public void stateAddHighestTransition() {
+        state = new State("q0");
+
+        ArrayList<State> states = new ArrayList<State>();
+        ArrayList<PatternMatcher> matchers = new ArrayList<PatternMatcher>();
+
+        for (int i = 0; i < 30; i++) {
+            String identifier = "q" + Integer.toString(i);
+
+            if (i == 29) {
+                states.add(0, new State(identifier));
+                matchers.add(0, new EpsilonMatcher());
+                state.addHighestTransition(states.get(0), matchers.get(0));
+            } else {
+                states.add(new State(identifier));
+                matchers.add(new CharacterMatcher(identifier.charAt(1)));
+                state.addTransition(states.get(i), matchers.get(i));
+            }
+        }
+
+        ArrayList<Transition> transitions = state.getTransitions();
+
+        for (int i = 0; i < 30; i++) {
+            assertEquals(states.get(i), transitions.get(i).getState());
+            assertEquals(matchers.get(i), transitions.get(i).getPatternMatcher());
+        }
+
+    }
+
+}
